d5057284e962ae2a320b384026aff18613614a66
KONATE Bakary <boukymen@gmail.com>
2023-09-25 03:34:17 +0000

    2023 : update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,7 +5,7 @@ import ReCAPTCHA from "react-google-recaptcha";
 
 export default function Contact() {
     const recaptcha = useRef();
-  const [message, setMessage] = useState(false);
+    const [message, setMessage] = useState(false);
 
     async function submitForm(event) {
         event.preventDefault();
@@ -13,49 +13,59 @@ export default function Contact() {
         if (!captchaValue) {
             alert("Please verify the reCAPTCHA!");
         } else {
-            const res = await fetch(`${process.env.API_ROOT_DOMAIN}/verify`, {
-                method: "POST",
-                body: JSON.stringify({ captchaValue }),
-                headers: {
-                    "content-type": "application/json",
-                },
-            });
-            const data = await res.json();
-            if (data.success) {
-                // make form submission
-                setMessage(true)
-                emailjs.sendForm('service_ofo2bvk', 'template_fo9iq6t', event.target, 'user_fBgdUIWbhaeRSN60VMbW2')
-                    .then((result) => {
-                        console.log(result.text);
-                    }, (error) => {
-                        console.log(error.text);
-                    });
+            try{
 
-                event.target.reset();
-            } else {
-                alert("reCAPTCHA validation failed!");
+                const res = await fetch(`${process.env.API_ROOT_DOMAIN}/verify`, {
+                    method: "POST",
+                    body: JSON.stringify({captchaValue}),
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                });
+                const data = await res?.json();
+                console.log(data)
+
+                if (data.success) {
+                    // make form submission
+                    setMessage(true)
+                    emailjs.sendForm('service_ylit6pf', 'template_b872x3o', event.target, 'user_fBgdUIWbhaeRSN60VMbW2')
+                        .then((result) => {
+                            console.log(result.text);
+                        }, (error) => {
+                            console.log(error.text);
+                        });
+
+                    event.target.reset();
+                } else {
+                    alert("reCAPTCHA validation failed!");
+                }
+            } catch (e) {
+                console.log(e)
             }
         }
     }
-  return (
-    <div className="contact" id="contact">
-      <h1>Contact us</h1>
-      <div className="left">
-        <img src="assets/shake.svg" alt="" />
-      </div>
 
-      <div className="right">
-        <form onSubmit={submitForm}>
-          <input required type="email" placeholder="Email" name="email" />
-          <input required type="name" placeholder="Name" name="name" />
-          <textarea className="text" required placeholder="Enter your message here" name="message"></textarea>
-            <ReCAPTCHA ref={recaptcha} sitekey={process.env.REACT_APP_SITE_KEY} />
-            <button type="submit" className="btn"> Send </button>
-            {message && <span>Thanks, I'll reply ASAP 😊</span>}
-        </form>
-      </div>
-    </div>
-  )
+    return (
+        <div className="contact" id="contact">
+            <h1>Contact us</h1>
+            <div className="left">
+                <img src="assets/shake.svg" alt=""/>
+            </div>
+
+            <div className="right">
+                <form onSubmit={submitForm} style={{height: "500px",}}>
+                    <input required type="email" placeholder="Email" name="email"
+                           style={{borderRadius: 10, padding: 2}}/>
+                    <input required type="name" placeholder="Name" name="name" style={{borderRadius: 10, padding: 2}}/>
+                    <textarea className="text" required placeholder="Enter your message here..."
+                              style={{borderRadius: 10, padding: 2}} name="message"></textarea>
+                    <ReCAPTCHA ref={recaptcha} sitekey={process.env.REACT_APP_SITE_KEY}/>
+                    <button type="submit" className="btn"> Send</button>
+                    {message && <span>Thanks, I'll reply ASAP 😊</span>}
+                </form>
+            </div>
+        </div>
+    )
 }
 //
 // const handleSubmit = (e) =>{
